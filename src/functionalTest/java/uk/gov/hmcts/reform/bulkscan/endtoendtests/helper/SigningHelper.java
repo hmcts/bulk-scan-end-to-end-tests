d8e9c7c56bd1c8514187acf9cd7a8a290c1ffd3b
Nitin Prabhu <prabhunitin61@gmail.com>
2020-06-02 23:06:27 +0100

    Load test key from resources instead of vault

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/helper/SigningHelper.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/helper/SigningHelper.java
@@ -1,19 +1,18 @@
 package uk.gov.hmcts.reform.bulkscan.endtoendtests.helper;
 
-import com.typesafe.config.ConfigFactory;
-
 import java.security.KeyFactory;
 import java.security.Signature;
 import java.security.spec.PKCS8EncodedKeySpec;
 
+import static com.google.common.io.Resources.getResource;
+import static com.google.common.io.Resources.toByteArray;
+
 public final class SigningHelper {
 
-    private static final String key = ConfigFactory.load().getString("signing-key-der");
-
     public static byte[] sign(byte[] input) throws Exception {
-
-        var signature = Signature.getInstance("SHA256withRSA");
-        signature.initSign(KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(key.getBytes())));
+        byte[] keyBytes = toByteArray(getResource("test_private_key.der"));
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(keyBytes)));
         signature.update(input);
         return signature.sign();
     }
