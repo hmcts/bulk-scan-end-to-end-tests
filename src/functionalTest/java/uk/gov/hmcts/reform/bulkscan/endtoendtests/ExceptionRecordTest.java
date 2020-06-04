8f6a69a85d5d8d589063ab6b173ce081feffa23d
Rafal Kondratowicz <rkondr@gmail.com>
2020-06-04 12:07:07 +0200

    remove not needed comments

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/ExceptionRecordTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/endtoendtests/ExceptionRecordTest.java
@@ -13,14 +13,12 @@ public class ExceptionRecordTest {
     public void should_upload_blob_and_create_exception_record() throws Exception {
         String zipFileName = ZipFileHelper.randomFileName();
 
-        // create zip file
         var zipArchive = ZipFileHelper.createZipArchive(
             singletonList("test-data/exception/1111002.pdf"),
             "test-data/exception/exception_metadata.json",
             zipFileName
         );
 
-        // upload zip file
         StorageHelper.uploadZipFile("bulkscan", zipFileName, zipArchive);
 
         Await.envelopeDispatched(zipFileName);
